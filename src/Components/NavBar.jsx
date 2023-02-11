05e130bd8f27bf61bc59cc068db8225a21545101
amirk1998 <amirk1998.pv@gmail.com>
2023-02-11 19:29:58 +0330

    Filter Todos has been creadted

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -1,18 +1,31 @@
-const NavBar = ({ unCompletedTodos }) => {
+import Select from 'react-select';
+
+const NavBar = ({ unCompletedTodos, onChange, selectedOption }) => {
+  const options = [
+    { value: 'All', label: 'All' },
+    { value: 'Completed', label: 'Completed' },
+    { value: 'Uncompleted', label: 'Uncompleted' },
+  ];
+
+  if (!unCompletedTodos) return <h2>Set your today tasks</h2>;
   return (
-    <header className='flex items-center justify-center w-full text-slate-800 text-center text-3xl my-4 rounded-lg '>
-      {unCompletedTodos ? (
-        <>
-          {' '}
-          <span className='mr-4 w-7 h-7 text-violet-700  text-lg bg-indigo-100 rounded-full flex justify-center items-center'>
-            {unCompletedTodos}
-          </span>{' '}
-          <h2>task(s) are not completed</h2>
-        </>
-      ) : (
-        <h2>Set your today tasks</h2>
-      )}
-    </header>
+    <div className='flex flex-col items-center justify-center mb-2 h-32'>
+      <div className='flex  items-center justify-center w-full text-slate-800 text-center text-3xl my-4 rounded-lg '>
+        <span className='mr-4 w-7 h-7 text-violet-700  text-lg bg-indigo-100 rounded-full flex justify-center items-center'>
+          {unCompletedTodos}
+        </span>
+        <h2>task(s) are not completed</h2>
+      </div>
+      <div className='flex items-center justify-between'>
+        <p className='mr-4'>Filter Todos:</p>
+        <Select
+          value={selectedOption}
+          onChange={onChange}
+          options={options}
+          className='w-32'
+        />
+      </div>
+    </div>
   );
 };
 
--- src/Components/TodoApp.jsx
+++ src/Components/TodoApp.jsx
@@ -1,10 +1,17 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+import Filter from './Filter';
 import NavBar from './NavBar';
 import TodoForm from './TodoForm';
 import TodoList from './TodoList';
 
 const TodoApp = () => {
   const [todos, setTodos] = useState([]);
+  const [filterTodos, setFilterTodos] = useState([]);
+  const [selectedOption, setSelectedOption] = useState('All');
+
+  useEffect(() => {
+    filterTodoFunction(selectedOption.value);
+  }, [todos, selectedOption]);
 
   const addTodo = (input) => {
     const newTodo = {
@@ -44,13 +51,36 @@ const TodoApp = () => {
     setTodos(updatedTodos);
   };
 
+  const filterTodoFunction = (status) => {
+    switch (status) {
+      case 'Completed':
+        setFilterTodos(todos.filter((t) => t.isCompleted));
+        break;
+      case 'Uncompleted':
+        setFilterTodos(todos.filter((t) => !t.isCompleted));
+        break;
+      default:
+        setFilterTodos(todos);
+    }
+  };
+
+  const selectHandler = (e) => {
+    console.log(e);
+    setSelectedOption(e);
+    filterTodoFunction(e.value);
+  };
+
   return (
     <div className='flex flex-col items-center'>
-      <NavBar unCompletedTodos={todos.filter((t) => !t.isCompleted).length} />
+      <NavBar
+        unCompletedTodos={todos.filter((t) => !t.isCompleted).length}
+        selectedOption={selectedOption}
+        onChange={selectHandler}
+      />
       <div className='flex flex-col items-center w-[40rem] bg-white border-[1px] border-solid border-stone-300 rounded-[5px] p-2 text-lg'>
         <TodoForm submitTodo={addTodo} />
         <TodoList
-          todos={todos}
+          todos={filterTodos}
           onComplete={completeTodo}
           onDelete={removeTodo}
           onUpdateTodo={updateTodo}
