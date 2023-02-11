cf5ce8bdd7afee51e4e595401a5cd1d19f9d6a8b
amirk1998 <amirk1998.pv@gmail.com>
2023-02-11 12:15:13 +0330

    Navbar has been creadted

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import TodoApp from './Components/TodoApp';
 
 function App() {
   return (
-    <div className='w-full flex flex-col items-center mt-5 p-3'>
+    <div className='w-full h-screen flex flex-col items-center mt-5 p-3 bg-gray-200'>
       <h1 className='mb-8 text-4xl font-bold '>TodoList App</h1>
       <TodoApp />
     </div>
@@ -12,5 +12,7 @@ function App() {
 
 export default App;
 
-//TODO=> E84
+//TODO=> E88
+
+// TODO => Exercise E87 => use react-select for filter => All , completed , uncompleted
 //Review => Edit E83
